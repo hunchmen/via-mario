908d45f510aed4515eaf32ba06ef7a6e23ef52d0
hunchmen <valalberto0214@gmail.com>
2022-04-07 20:54:59 +0800

    initial commit with window implementation

---
+++ src/main/java/com/via/jade/Window.java
@@ -0,0 +1,133 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+package com.via.jade;
+
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_MAXIMIZED;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11C.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11C.glClear;
+import static org.lwjgl.opengl.GL11C.glClearColor;
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+
+/**
+ * @author 63999
+ * @date Apr 5, 2022
+ */
+public class Window {
+
+  // Screen width size
+  int width;
+
+  // Screen height size
+  int height;
+
+  // Title
+  String title;
+
+  private long glfwWindow;
+
+  /**
+   * Initialize window for singleton pattern
+   */
+  private static Window window = null;
+
+  private Window() {
+    this.width = 1920;
+    this.height = 1080;
+    this.title = "Mario";
+  }
+
+  /**
+   * Singleton implementation of Window
+   * 
+   * @return window
+   */
+  public static Window get() {
+    if (Window.window == null) {
+      Window.window = new Window();
+    }
+
+    return Window.window;
+  }
+
+  public void run() {
+    System.out.println("Hello LWJGL" + Version.getVersion());
+
+    init();
+    loop();
+  }
+
+  public void init() {
+    // Setup an error callback
+    GLFWErrorCallback.createPrint(System.err).set();
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+      throw new IllegalStateException("Unable to initialize GLFW.");
+    }
+
+    // Configure GLFW
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+    // Create the window
+    glfwWindow = glfwCreateWindow(this.width, this.height, this.title, 0, 0);
+
+    if (glfwWindow == 0) { // need to check if null is deference from 0
+      throw new IllegalStateException("Failed to create GLFW window");
+    }
+
+    // Make the OPENGL context current
+    glfwMakeContextCurrent(glfwWindow);
+
+    // Enable v-sync
+    glfwSwapInterval(1);
+
+    // Make the window visible
+    glfwShowWindow(glfwWindow);
+
+    // This line is critical for LWJGL's interoperation with GLFW's
+    // OpenGL context, or any context that is managed externally.
+    // LWJGL detects the context that is current in the current thread,
+    // creates the GLCapabilities instance and makes the OpenGL
+    // bindings available for use.
+    GL.createCapabilities();
+  }
+
+  public void loop() {
+
+    while (!glfwWindowShouldClose(glfwWindow)) {
+      // Poll events
+      glfwPollEvents();
+
+      glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glfwSwapBuffers(glfwWindow);
+    }
+
+  }
+
+}
---
+++ src/main/java/com/via/main/Main.java
@@ -0,0 +1,28 @@
+package com.via.main;
+
+import com.via.jade.Window;
+
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+
+/**
+ * 
+ * 
+ * @author 63999
+ * 
+ * 
+ *         Apr 5, 2022
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Window window = Window.get();
+    window.run();
+
+  }
+
+}
